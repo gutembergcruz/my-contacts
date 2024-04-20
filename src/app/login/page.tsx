473caf7b53a2305fe97fd564900d3ea1d00c828d
Gutemberg Cruz <gutemberg.cruz@profranchising.com.br>
2024-04-20 08:58:46 -0300

    inputText

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,15 @@
 import { InputText } from "@/components/InputText";
-// import "./styles.scss";
+
+import Styles from "./login.module.scss";
 
 export default function Login() {
   return (
-    <div className="container">
-      <section className="box-login">
-        <InputText />
-        <InputText />
+    <div className={Styles.container}>
+      <h1>My Contacts</h1>
+      <section className={Styles.boxLogin}>
+        <p>Login</p>
+        <InputText label="Email" />
+        <InputText type="password" label="Password" />
         <button>Login</button>
       </section>
     </div>
--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,8 +1,38 @@
+'use client'
+import { FiXCircle } from 'react-icons/fi';
+import Styles from './InputText.module.scss'
+import { useState } from 'react';
 
-export function InputText() {
+interface InputProps {
+    name?: string;
+    label: string;
+    value?: string;
+    type?: string;
+    onChange?: (value: string) => void;
+}
+export function InputText({ name, label, value, onChange, type }: InputProps) {
+    const [inputValue, setInputValue] = useState(value || '');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value;
+        setInputValue(newValue);
+        if (onChange) {
+            onChange(newValue);
+        }
+    };
+
+    const handleClear = () => {
+        setInputValue('');
+        if (onChange) {
+            onChange('');
+        }
+    };
+
     return (
-        <div>
-            <input type="text" className="teste" />
+        <div className={Styles.container}>
+            <input name={name} required type={type === 'password' ? 'password' : 'text'} value={inputValue} onChange={handleChange}  />
+            <label htmlFor="">{label}</label>
+            <button onClick={handleClear}> <FiXCircle size={20} /> </button>
         </div>
     );
 }
